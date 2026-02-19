373c5448c6acc1f7957013464280429b65b74883
David <alejo21ny@hotmail.com>
2026-02-19 01:38:21 -0500

    Initial plugin structure and main plugin file

---
+++ education-resources-manager.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: Education Resources Manager
+ * Plugin URI: https://github.com/alejo21ny/education-resources-manager
+ * Description: Educational resource management system.
+ * Version: 1.0.0
+ * Author: David Gomez
+ * License: GPL2+
+ */
+
+if (!defined('ABSPATH')) {
+    exit;
+}
